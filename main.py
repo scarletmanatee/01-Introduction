9ba123dce0c524b449f4aa0924ef9333cd167bf1
Beer <ambeer@iu.edu>
2019-08-28 17:11:58 -0400

    wrote answers into main.py and edited LICENSE

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name in Andy Beer.')
+
+print('My favorite game is Fez, by polytron studios.')
+
+print('I hope this class is not too boring because I have taken a previous interation of it.')
+
+print('I am excited about getting home tonight because I just rememberd I have Indian food in my fridge!')
+
+print('11980668')
+
+print('https://github.com/scarletmanatee')
